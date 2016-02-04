a6ba067900cc9ae9a4e50fcb53c111d59ae8085f
Max Sun <maxsun@Hawk.local>
2016-02-03 20:35:07 -0800

    Fully working, without textures.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//http://www.permadi.com/tutorial/raycast/rayc8.html
+
+
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var canvasscreen = document.getElementById("screen");
@@ -9,11 +12,12 @@ window.addEventListener("keyup", keyup_handler, false);
 var world= [
             [0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0],
             [0, 1, 0, 0, 0],
             [0, 0, 0, 0, 0]
 ];
 var unitSize = canvas.width/world.length;
+var rayHitWidth = 5;
 var screenUnitSize = canvasscreen.width;
 
 var speed = 3;
@@ -39,24 +43,67 @@ function castRay(originx, originy, angle){
                 var xmax = (x+1)*unitSize;
                 var ymin = y*unitSize;
                 var ymax = (y+1)*unitSize;
-                //check if our line collides with these mins/maxs
-                // y1=m(x2-x1)+y2
+
                 var xminInt = slope*(xmin-originx)+originy;
                 var xmaxInt = slope*(xmax-originx)+originy;
                 var yminInt = (ymin-originy)/slope+originx;
                 var ymaxInt = (ymax-originy)/slope+originx;
 
                 if(xminInt < ymax && xminInt > ymin){
-                        results.push([xmin, xminInt]);
+                    if(angle >= 270 && xmin >= originx && xminInt <= originy){
+                        results.push([xmin, xminInt, angle]);
+                    }
+                    if(angle <= 90 && xmin >= originx && xminInt >= originy){
+                        results.push([xmin, xminInt, angle]);
+                    }
+                    if(angle >= 90 && angle <= 180 && xmin <= originx && xminInt >= originy){
+                        results.push([xmin, xminInt, angle]);
+                    }
+                    if(angle >= 180 && angle <= 270 && xmin <= originx && xminInt <= originy){
+                        results.push([xmin, xminInt, angle]);
+                    }
                 }
                 if(xmaxInt < ymax && xmaxInt > ymin){
-                        results.push([xmax, xmaxInt]);
+                    if(angle >= 270 && xmax >= originx && xmaxInt <= originy){
+                        results.push([xmax, xmaxInt, angle]);
+                    }
+                    if(angle <= 90 && xmax >= originx && xmaxInt >= originy){
+                        results.push([xmax, xmaxInt, angle]);
+                    }
+                    if(angle >= 90 && angle <= 180 && xmax <= originx && xmaxInt >= originy){
+                        results.push([xmax, xmaxInt, angle]);
+                    }
+                    if(angle >= 180 && angle <= 270 && xmax <= originx && xmaxInt <= originy){
+                        results.push([xmax, xmaxInt, angle]);
+                    }
                 }
                 if(ymaxInt < xmax && ymaxInt > xmin){
-                        results.push([ymaxInt, ymax]);
+                    if(angle >= 270 && ymaxInt >= originx && ymax <= originy){
+                        results.push([ymaxInt, ymax, angle]);
+                    }
+                    if(angle <= 90 && ymaxInt >= originx && ymax >= originy){
+                        results.push([ymaxInt, ymax, angle]);
+                    }
+                    if(angle >= 90 && angle <= 180 && ymaxInt <= originx && ymax >= originy){
+                        results.push([ymaxInt, ymax, angle]);
+                    }
+                    if(angle >= 180 && angle <= 270 && ymaxInt <= originx && ymax <= originy){
+                        results.push([ymaxInt, ymax, angle]);
+                    }
                 }
                 if(yminInt < xmax && yminInt > xmin){
-                        results.push([yminInt, ymin]);
+                    if(angle >= 270 && yminInt >= originx && ymin <= originy){
+                        results.push([yminInt, ymin, angle]);
+                    }
+                    if(angle <= 90 && yminInt >= originx && ymin >= originy){
+                        results.push([yminInt, ymin, angle]);
+                    }
+                    if(angle >= 90 && angle <= 180 && yminInt <= originx && ymin >= originy){
+                        results.push([yminInt, ymin, angle]);
+                    }
+                    if(angle >= 180 && angle <= 270 && yminInt <= originx && ymin <= originy){
+                        results.push([yminInt, ymin, angle]);
+                    }
                 }
             }
 
@@ -66,22 +113,34 @@ function castRay(originx, originy, angle){
 }
 
 function keyup_handler(event) {
-    if (event.keyCode == 87 || event.keyCode == 83) {
+    if (event.keyCode == 87 || event.keyCode == 83 || event.keyCode == 65 || event.keyCode == 68) {
         player.mod = 0;
     }
 }
 
+var direction;
+
 function keypress_handler(event) {
     if (event.keyCode == 87) {
+        direction = "forwards";
         player.mod = 1;
     }
     if (event.keyCode == 83) {
-        player.mod = -1;
+        direction = "backwards";
+        player.mod = 1;
     }
     if (event.keyCode == 65) {
-        player.dir -= 1;
+        direction = "left"
+        player.mod = 1;
     }
     if (event.keyCode == 68) {
+        direction = "right"
+        player.mod = 1;
+    }
+    if (event.keyCode == 37) {
+        player.dir -= 1;
+    }
+    if (event.keyCode == 39) {
         player.dir += 1;
     }
 }
@@ -96,14 +155,21 @@ function draw(){
     if(player.dir > 360){
         player.dir -= 360;
     }
-    player.x += 1 * (speed * player.mod) * Math.cos(Math.PI / 180 * (player.dir));
-    player.y += 1 * (speed * player.mod) * Math.sin(Math.PI / 180 * (player.dir));
+    xchange = 1 * (speed * player.mod) * Math.cos(Math.PI / 180 * (player.dir));
+    ychange = 1 * (speed * player.mod) * Math.sin(Math.PI / 180 * (player.dir));
+ 
+    if(direction == "forwards"){player.x += xchange; player.y += ychange;}
+    if(direction == "backwards"){player.x -= xchange; player.y -= ychange;}
+    if(direction == "left"){player.y -= xchange; player.x += ychange;}
+    if(direction == "right"){player.y += xchange; player.x -= ychange;}
+
     context.clearRect(0, 0, 1000, 1000);
     contextscreen.clearRect(0,0,1000,1000);
     context.fillStyle = "rgb(200, 200, 255)"
-    contextscreen.moveTo(0,canvasscreen.height/2);
-    contextscreen.lineTo(canvasscreen.width, canvasscreen.height/2);
-    contextscreen.stroke();
+    // contextscreen.moveTo(0,canvasscreen.height/2);
+    // contextscreen.lineTo(canvasscreen.width, canvasscreen.height/2);
+    // contextscreen.stroke();
+
     for(var y = 0; y < world.length; y++){
         for(var x = 0; x < world[y].length; x++){
             if(world[y][x] != 0){
@@ -111,7 +177,11 @@ function draw(){
             }
         }
     }
-    context.fillStyle = "rgb(0, 0, 0ww)";
+    contextscreen.fillStyle = "rgb(115, 93, 30)";
+    contextscreen.fillRect(0, canvasscreen.height/2, canvasscreen.width, canvasscreen.height/2);
+    contextscreen.fillStyle = "rgb(187, 234, 250)";
+    contextscreen.fillRect(0, 0, canvasscreen.width, canvasscreen.height/2);
+    context.fillStyle = "rgb(0, 0, 0)";
     var hits = [];
     for(var ang = player.dir-(fov/2); ang < player.dir+(fov/2); ang+=.1){
         hits = hits.concat(castRay(player.x, player.y, ang));
@@ -120,13 +190,14 @@ function draw(){
     contextscreen.fillStyle = "rgb(0, 0, 255)"
     for(var i = 0; i < hits.length; i++){
         var h = hits[i];
-        var ang = toDeg(Math.atan((h[1]-player.y)/(h[0]-player.x)));
-        var xloc = canvasscreen.width*(((player.dir-ang)/fov)+.5);
-        var distance = Math.sqrt(Math.pow(player.x-h[0]*screenUnitSize,2) + Math.pow(player.y-h[1]*screenUnitSize,2));
-        distance = distance * Math.cos(player.dir-ang);
-        var ph = Math.round()
-
-        contextscreen.fillRect(xloc, canvasscreen.height/2-ph/2, 8, ph);
+        var ang = h[2];
+        var xloc = canvasscreen.width*((ang-player.dir)/fov);
+        xloc += canvasscreen.width/2;
+        var distance = Math.sqrt(Math.pow(player.x-h[0],2) + Math.pow(player.y-h[1],2));
+        var perpDist = distance*Math.cos(toRad(player.dir-ang));
+        var ph = screenUnitSize/perpDist;
+        ph *= screenUnitSize/9;
+        contextscreen.fillRect(xloc, canvasscreen.height/2-ph/2, rayHitWidth, ph);
     }
 
     for(var i = 0; i < hits.length; i++){
